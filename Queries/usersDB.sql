a3944adefcbf59fc7fd38a8ac132300617e52aba
AlineCrawf <aline8crawford@gmail.com>
2020-11-13 21:18:57 +0200

    DB for users

---
+++ Queries/usersDB.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE Users
+with 
+OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Ukraine.1251'
+    LC_CTYPE = 'Russian_Ukraine.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
